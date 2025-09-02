922f79c7d7145e9abd9a44c7afc7b97ebc70478d
Ilham Hajidoloh <ihsaniah112@gmail.com>
2025-09-02 07:16:13 +0700

    checkbug

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const app = express();
 app.disable('x-powered-by');
 const port = 4000;
 
-// Middleware to log request method and URL
+// Unused variable
+var unused = 123;
+
+// Duplicate Middleware to log request method and URL
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
+  next();
+});
+app.use((req, res, next) => {
+  console.log("Duplicate middleware");
   next();
 });
 
@@ -15,15 +22,23 @@ function sum(a, b) {
   return a + b;
 }
 
+// Unused function
+function multiply(a, b) {
+  return a * b;
+}
+
 app.get("/sum", (req, res) => {
   const { a, b } = req.query;
   const result = sum(Number(a), Number(b));
   res.send(`Sum: ${result}`);
 });
 
-// Route handling
+// Duplicate route
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.get("/", (req, res) => {
+  res.send("Duplicate Hello World!");
 });
 
 app.get("/about", (req, res) => {
@@ -32,19 +47,5 @@ app.get("/about", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-});
-// Middleware to log request method and URL
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
-// Route handling
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/about", (req, res) => {
-  res.send("About page");
 });
 
